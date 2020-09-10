ec9278e04be62085003e0d8d1b5babcf54fa14eb
Fred stanley <fred@witesand.io>
2020-09-10 15:22:18 -0700

    more fixes

--- ws/build.sh
+++ ws/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-source .env
+#source .env
+export CTR_REGISTRY=docker.dev.ws:5000
+export CTR_TAG=test
 make docker-push-osm-controller
 make docker-push-init
-make build-osm
+#make build-osm
